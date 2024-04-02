585c0df0892ae8e6fcb7e5171cf7440d5c3b219a
Twincasper <justinwatson651@gmail.com>
2024-04-02 12:26:31 -0400

    Completed server application requirements

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const distFolder = path.join(__dirname, '..', 'app', 'dist');
+
+const logRoutes = (req, res, next) => {
+  const time = (new Date()).toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next();
+};
+
+const serveStatic = express.static(distFolder);
+
+app.use(logRoutes);
+app.use(serveStatic);
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is now running on http://localhost:${port}`);
+});
